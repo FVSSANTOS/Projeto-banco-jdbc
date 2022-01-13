53ee7ad5ab184558ac33edafb512ed0830f3afa8
Vinicius <flaviovss@yahoo.com>
2022-01-13 09:37:10 -0300

    Classe que representa a conta poupança

---
+++ Poupanca.java
@@ -0,0 +1,9 @@
+package ProjetoBanco;
+
+public class Poupança extends Conta{
+
+	public Poupança(String numero, Agencia agencia, Cliente cliente, Double saldo) {
+		super(numero, agencia, cliente, saldo);
+	}
+
+}
