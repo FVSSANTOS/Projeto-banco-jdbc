220b6a4da6ae278eb6e4fee77e2cae3ce94731c1
Vinicius <flaviovss@yahoo.com>
2022-01-13 09:36:23 -0300

    .

---
+++ ContaCorrente.java
@@ -0,0 +1,10 @@
+package ProjetoBanco;
+
+public class ContaCorrente extends Conta{
+
+	public ContaCorrente(String numero, Agencia agencia, Cliente cliente, Double saldo) {
+		super(numero, agencia, cliente, saldo);
+	}
+
+	
+}
